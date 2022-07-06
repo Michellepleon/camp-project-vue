89800936d0fae79c263d6583bb607214714d36c1
Michelle Pinar <michelle_pleon@hotmail.com>
2022-07-05 20:04:07 -0400

    added vuetify + eslint + airbnb + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,28 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+    es6: true,
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "airbnb-base",
+    "prettier",
   ],
+  plugins: ["prettier"],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: "@babel/eslint-parser",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": "error",
+    "no-unused-vars": "warn",
+    "func-names": "off",
+    "no-process-exit": "off",
+    "object-shorthand": "off",
+    "class-methods-use-this": "off",
+  },
+};
