9babb88eac8c94eb4400cbc71bd4e3d6e59bca9e
Michelle Pinar <michelle-pinar@Michelles-MacBook-Air.local>
2022-08-24 22:23:44 -0400

    Add the new routes Extreme and Arts and Science

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+// All the
 import HomeView from "../views/HomeView.vue";
 import ClassicView from "../views/ClassicView.vue";
+import ExtremeView from "../views/ExtremeView.vue";
+import ArtsSienceView from "../views/ArtsSienceView.vue";
 
 Vue.use(VueRouter);
 
@@ -16,9 +19,21 @@ const routes = [
     name: "Classic",
     component: ClassicView,
   },
+  {
+    path: "/extreme",
+    name: "Extreme",
+    component: ExtremeView,
+  },
+  {
+    path: "/arts&sience",
+    name: "Arts&Sience",
+    component: ArtsSienceView,
+  },
 ];
 
 const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
   routes,
 });
 
