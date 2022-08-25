fcb7923a38f94c96d7c41ac17ebcd865c4dcb7db
Michelle Pinar <michelle-pinar@Michelles-MacBook-Air.local>
2022-08-24 22:04:11 -0400

    NavBar logo fixed

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,10 +1,13 @@
 <template>
-  <v-app-bar app clipped-left src="@/assets/bannerok.jpeg" height="100">
+  <v-app-bar app clipped-left src="@/assets/bannerok.jpeg" height="85">
     <v-app-bar-nav-icon>
-      <v-img src="@/assets/Logo.png" width="90" height="90" />
+      <v-img
+        src="@/assets/Logosm.png"
+        class="px-5 ml-8"
+        height="80"
+        width="90"
+      />
     </v-app-bar-nav-icon>
-
-    <v-app-bar-title class="black--text">Welcome</v-app-bar-title>
   </v-app-bar>
 </template>
 
